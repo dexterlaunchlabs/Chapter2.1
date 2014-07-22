d1ec4f06d972c781a6318d08718c1b299edeb5ad
Dexter <dexter@launchlabs.ph>
2014-07-22 15:11:38 +0800

    Hello World App

---
+++ Hello World/Hello World/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Dexter Launchlabs on 7/22/14.
+//  Copyright (c) 2014 Dexter Launchlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
